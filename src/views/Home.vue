010d1274029d96c6497657708a61bc8acfd5786d
wcgod <wcgod90@gmail.com>
2021-07-07 01:27:37 +0900

    vue.config.js 수정

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,18 +11,15 @@
                   <div class="container">
                       <div class="row p-5">
                           <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                              <img class="img-fluid" src="img/banner_img_01.jpg" alt="">
+                              <img class="img-fluid" src="img/profile_01.jpg" alt="">
                           </div>
                           <div class="col-lg-6 mb-0 d-flex align-items-center">
                               <div class="text-align-left align-self-center">
-                                  <h1 class="h1 text-success"><b>Zay</b> eCommerce</h1>
-                                  <h3 class="h2">Tiny and Perfect eCommerce Template</h3>
+                                  <h1 class="h1 text-success"><b>도전하는 개발자</b></h1>
+                                  <h3 class="h2">어느 곳을 가든 지금 있는 그곳에서부터 길이 시작된다.</h3>
                                   <p>
-                                      Zay Shop is an eCommerce HTML5 CSS template with latest version of Bootstrap 5 (beta 1).
-                                      This template is 100% free provided by <a rel="sponsored" class="text-success" href="https://templatemo.com" target="_blank">TemplateMo</a> website.
-                                      Image credits go to <a rel="sponsored" class="text-success" href="https://stories.freepik.com/" target="_blank">Freepik Stories</a>,
-                                      <a rel="sponsored" class="text-success" href="https://unsplash.com/" target="_blank">Unsplash</a> and
-                                      <a rel="sponsored" class="text-success" href="https://icons8.com/" target="_blank">Icons 8</a>.
+                                      다양한 업무,신기술,새로운 서비스를 두려워하지 않고 도전.</br>
+                                      자신의 길은 자기가 만들듯이, 새로운 도전을 통한 앞으로의 자신을 만들어간다.
                                   </p>
                               </div>
                           </div>
@@ -33,15 +30,15 @@
                   <div class="container">
                       <div class="row p-5">
                           <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                              <img class="img-fluid" src="img/banner_img_02.jpg" alt="">
+                              <img class="img-fluid" src="img/profile_02.jpg" alt="">
                           </div>
                           <div class="col-lg-6 mb-0 d-flex align-items-center">
                               <div class="text-align-left">
-                                  <h1 class="h1">Proident occaecat</h1>
-                                  <h3 class="h2">Aliquip ex ea commodo consequat</h3>
+                                  <h1 class="h1 text-success"><b>활동으로 배우는 삶</b></h1>
+                                  <h3 class="h2">인생은 경험의 연속이다.</h3>
                                   <p>
-                                      You are permitted to use this Zay CSS template for your commercial websites.
-                                      You are <strong>not permitted</strong> to re-distribute the template ZIP file in any kind of template collection websites.
+                                      아르바이트, 대외활동, 봉사활동, 취미 등 다양한 활동 경험.</br>
+                                      활동을 통해 지식과 지혜를 배우고, 자기 자신을 만들어가는 과정.
                                   </p>
                               </div>
                           </div>
@@ -52,15 +49,15 @@
                   <div class="container">
                       <div class="row p-5">
                           <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                              <img class="img-fluid" src="img/banner_img_03.jpg" alt="">
+                              <img class="img-fluid" src="img/profile_03.jpg" alt="">
                           </div>
                           <div class="col-lg-6 mb-0 d-flex align-items-center">
                               <div class="text-align-left">
-                                  <h1 class="h1">Repr in voluptate</h1>
-                                  <h3 class="h2">Ullamco laboris nisi ut </h3>
+                                  <h1 class="h1 text-success"><b>효율적인 시간 사용</b></h1>
+                                  <h3 class="h2">행운은 계획에서 비롯된다.</h3>
                                   <p>
-                                      We bring you 100% free CSS templates for your websites.
-                                      If you wish to support TemplateMo, please make a small contribution via PayPal or tell your friends about our website. Thank you.
+                                      우선순위를 가지고 계획된 업무 처리.</br>
+                                      꾸준한 계획을 통해 공부하고 성장하는 자기계발.
                                   </p>
                               </div>
                           </div>
@@ -100,9 +97,8 @@
                           <div class="card-body">
                               <a href="shop-single.html" class="h2 text-decoration-none text-dark">GSITM</a>
                               <p class="card-text">
-
+                              2017.02~2020.02 (3년 1개월)
                               </p>
-                              <p class="text-muted">경력 (3년 1개월)</p>
                           </div>
                       </div>
                   </div>
@@ -114,9 +110,8 @@
                           <div class="card-body">
                               <a href="shop-single.html" class="h2 text-decoration-none text-dark">현대아이티앤이</a>
                               <p class="card-text">
-
+                              2020.02~2021.07 (1년 6개월)
                               </p>
-                              <p class="text-muted">경력 (1년 6개월)</p>
                           </div>
                       </div>
                   </div>
