d2ee27e7502fb9d20c70d55ea4160234ff3c07b5
wcgod <wcgod90@gmail.com>
2021-07-07 02:10:26 +0900

    이미지 수정 및 shop 구성

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -1,7 +1,213 @@
 <template>
-  <div>
-    shop page
+  <!-- Start Content -->
+  <div class="container py-5">
+      <div class="row">
+
+          <div class="col-lg-3">
+              <h1 class="h2 pb-4">카테고리</h1>
+              <ul class="list-unstyled templatemo-accordion">
+                  <li class="pb-3">
+                      <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
+                          FrontEnd
+                          <i class="fa fa-fw fa-chevron-circle-down mt-1"></i>
+                      </a>
+                      <ul class="collapse show list-unstyled pl-3">
+                          <li><a class="text-decoration-none" href="#">Vuejs</a></li>
+                          <li><a class="text-decoration-none" href="#">Javascript</a></li>
+                      </ul>
+                  </li>
+                  <li class="pb-3">
+                      <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
+                          BackEnd
+                          <i class="pull-right fa fa-fw fa-chevron-circle-down mt-1"></i>
+                      </a>
+                      <ul id="collapseTwo" class="collapse list-unstyled pl-3">
+                          <li><a class="text-decoration-none" href="#">Java</a></li>
+                          <li><a class="text-decoration-none" href="#">Spring</a></li>
+                          <li><a class="text-decoration-none" href="#">Nexacro</a></li>
+                      </ul>
+                  </li>
+                  <li class="pb-3">
+                      <a class="collapsed d-flex justify-content-between h3 text-decoration-none" href="#">
+                          ETC
+                          <i class="pull-right fa fa-fw fa-chevron-circle-down mt-1"></i>
+                      </a>
+                      <ul id="collapseThree" class="collapse list-unstyled pl-3">
+                          <li><a class="text-decoration-none" href="#">Git</a></li>
+                          <li><a class="text-decoration-none" href="#">SQL</a></li>
+                          <li><a class="text-decoration-none" href="#">Python</a></li>
+                      </ul>
+                  </li>
+              </ul>
+          </div>
+
+          <div class="col-lg-9">
+              <div class="row">
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Java</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$100.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Javascript</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$100.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Spring</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$80.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">SQL</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$80.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Git</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>초중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$60.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Nexacro</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>중급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$80.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Vuejs</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>초급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$30.00</p>
+                          </div>
+                      </div>
+                  </div>
+                  <div class="col-md-4">
+                      <div class="card mb-4 product-wap rounded-0">
+                          <div class="card-body">
+                              <a href="shop-single.html" class="h3 text-decoration-none">Python</a>
+                              <ul class="w-100 list-unstyled d-flex justify-content-between mb-0">
+                                  <li>초급</li>
+                              </ul>
+                              <ul class="list-unstyled d-flex justify-content-center mb-1">
+                                  <li>
+                                      <i class="text-warning fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                      <i class="text-muted fa fa-star"></i>
+                                  </li>
+                              </ul>
+                              <p class="text-center mb-0">$30.00</p>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
   </div>
+  <!-- End Content -->
 </template>
 
 <script>
